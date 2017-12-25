9d7b07d5a64fb93fa5b54fa1faa9fda4841517ea
Alexander Smagin <alexander.v.smagin@gmail.com>
2017-12-25 18:50:07 -0500

    Add scripts to start test VMs

---
+++ tests/remove.ps1
@@ -0,0 +1,3 @@
+vagrant box remove w16s-scsql16d D:\.projects\oss\packer\build\w16s-scsql16d\virtualbox-core\output\vagrant.box
+vagrant box remove w16s-scsolr D:\.projects\oss\packer\build\w16s-scsolr\virtualbox-core\output\vagrant.box
+vagrant box remove w16s-sc90 D:\.projects\oss\packer\build\w16s-sc90\virtualbox-core\output\vagrant.box
