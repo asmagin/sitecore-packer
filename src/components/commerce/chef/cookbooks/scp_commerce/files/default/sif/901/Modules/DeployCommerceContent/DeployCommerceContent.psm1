774f60171baed4a079ee8ab704d4257f166484c7
Alex Smagin <alexander.v.smagin@gmail.com>
2018-05-24 18:46:08 -0400

    Update Commerce instalaltion scripts

--- src/components/commerce/chef/cookbooks/scp_commerce/files/default/sif/901/Modules/DeployCommerceContent/DeployCommerceContent.psm1
+++ src/components/commerce/chef/cookbooks/scp_commerce/files/default/sif/901/Modules/DeployCommerceContent/DeployCommerceContent.psm1
@@ -82,7 +82,7 @@ Function Invoke-DeployCommerceContentTask {
                 $originalJson = Get-Content $pathToJson -Raw  | ConvertFrom-Json
                 $originalJson.AppSettings.EnvironmentName = "AdventureWorksOpsApi"
 
-                $allowedOrigins = @($CommerceAuthoringBaseUri, $SiteBaseUri)
+                $allowedOrigins = @($SitecoreBizFxBaseUri, $SiteBaseUri)
                 $originalJson.AppSettings.AllowedOrigins = $allowedOrigins
                 $originalJson.AppSettings.SslPort = $CommerceOpsServicesPort
                 $originalJson.AppSettings.SitecoreIdentityServerUrl = $SitecoreIdentityServerBaseUri
@@ -189,11 +189,21 @@ Function Invoke-DeployCommerceContentTask {
                     $jsonFile = Get-ChildItem "$pathToEnvironmentFiles\PlugIn.Search.PolicySet*.json"
                     $json = Get-Content $jsonFile.FullName -Raw | ConvertFrom-Json
                     $indexes = @()
+                    
                     # Generically update the different search scope policies so it will be updated for any index that exists or is created in the future
                     Foreach ($p in $json.Policies.'$values') {
                         if ($p.'$type' -eq 'Sitecore.Commerce.Plugin.Search.SearchViewPolicy, Sitecore.Commerce.Plugin.Search') {
                             $oldSearchScopeName = $p.SearchScopeName
-                            $searchScopeName = "$SearchIndexPrefix$oldSearchScopeName"
+                            if ($p.SearchScopeName -eq "OrdersScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_orders_scope"
+                            }
+                            elseif ($p.SearchScopeName -eq "CustomersScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_customers_scope"
+                            }
+                            elseif ($p.SearchScopeName -eq "CatalogItemsScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_catalog_items_scope"
+                            }
+                            
                             $p.SearchScopeName = $searchScopeName;
                             $Writejson = $true;
 
@@ -205,7 +215,15 @@ Function Invoke-DeployCommerceContentTask {
 
                         if ($p.'$type' -eq 'Sitecore.Commerce.Plugin.Search.SearchScopePolicy, Sitecore.Commerce.Plugin.Search') {
                             $oldName = $p.Name
-                            $name = "$SearchIndexPrefix$oldName"
+                            if ($p.Name -eq "OrdersScope") {
+                                $name = "$($SearchIndexPrefix)_orders_scope"
+                            }
+                            elseif ($p.Name -eq "CustomersScope") {
+                                $name = "$($SearchIndexPrefix)_customers_scope"
+                            }
+                            elseif ($p.Name -eq "CatalogItemsScope") {
+                                $name = "$($SearchIndexPrefix)_catalog_items_scope"
+                            }
                             $p.Name = $name;
                             $Writejson = $true;
 
@@ -214,7 +232,16 @@ Function Invoke-DeployCommerceContentTask {
 
                         if ($p.'$type' -eq 'Sitecore.Commerce.Plugin.Search.IndexablePolicy, Sitecore.Commerce.Plugin.Search') {
                             $oldSearchScopeName = $p.SearchScopeName
-                            $searchScopeName = "$SearchIndexPrefix$oldSearchScopeName"
+
+                            if ($p.SearchScopeName -eq "OrdersScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_orders_scope"
+                            }
+                            elseif ($p.SearchScopeName -eq "CustomersScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_customers_scope"
+                            }
+                            elseif ($p.SearchScopeName -eq "CatalogItemsScope") {
+                                $searchScopeName = "$($SearchIndexPrefix)_catalog_items_scope"
+                            }
                             $p.SearchScopeName = $searchScopeName;
                             $Writejson = $true;
 
@@ -248,7 +275,7 @@ Function Invoke-DeployCommerceContentTask {
                 $pathToJson = $(Join-Path -Path $PhysicalPath -ChildPath "wwwroot\config.json")
                 $originalJson = Get-Content $pathToJson -Raw | ConvertFrom-Json
 
-                $allowedOrigins = @($SitecoreBizFxBaseUri)
+                $allowedOrigins = @($SitecoreBizFxBaseUri, $SiteBaseUri)
                 $originalJson.AppSettings.AllowedOrigins = $allowedOrigins
                 if ($_ -match "CommerceShops") {
                     $originalJson.AppSettings.SslPort = $CommerceShopsServicesPort
