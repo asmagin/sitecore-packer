db97987dbce51bf95db7c44b2998cfa8ce6f4166
Alex Smagin <alexander.v.smagin@gmail.com>
2018-05-29 22:00:06 -0400

    Fix configuration of SOLR service name in scripts

--- src/components/sql/chef/cookbooks/scp_sql/attributes/2016_developer.rb
+++ src/components/sql/chef/cookbooks/scp_sql/attributes/2016_developer.rb
@@ -1,3 +1,3 @@
 default['scp_sql']['2016_developer'] = {
-  'installer_iso_url' => 'https://download.my.visualstudio.com/pr/en_sql_server_2016_developer_with_service_pack_1_x64_dvd_9548071.iso?t=b0bb9d2a-70e7-48fc-b3a9-9c8bdceea334&e=1524664459&h=f2dd1f3346283ed4127db9f8ed762e1d&su=1',
+  'installer_iso_url' => 'https://download.my.visualstudio.com/pr/en_sql_server_2016_developer_with_service_pack_1_x64_dvd_9548071.iso?t=f3494298-254f-4040-8564-ae295e951694&e=1527664999&h=5d95fb5cbe26946363a21a5b0dc7872b&su=1',
 }
