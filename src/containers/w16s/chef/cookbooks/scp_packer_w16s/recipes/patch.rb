e51d97288812f38f9aa6930837e0bffc82ffaaf0
shsh <shishagin.alexey@gmail.com>
2018-12-09 20:10:50 +0300

    Implemented full credssp fix

--- src/containers/w16s/chef/cookbooks/scp_packer_w16s/recipes/patch.rb
+++ src/containers/w16s/chef/cookbooks/scp_packer_w16s/recipes/patch.rb
@@ -1,1 +1,13 @@
 include_recipe 'scp_packer_w::patch'
+
+scp_windows_updates 'Install update fixing Oracle encryption error' do
+  msu_source 'http://download.windowsupdate.com/d/msdownload/update/software/secu/2018/05/windows10.0-kb4103723-x64_2adf2ea2d09b3052d241c40ba55e89741121e07e.msu'
+  action :install
+end
+
+scp_windows_powershell_script_elevated 'Set Oracle encryption policy to mitigated' do
+  code <<-EOH
+    reg add "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\CredSSP\\Parameters" /v AllowEncryptionOracle /t REG_DWORD /d 2 /f
+  EOH
+  action :run
+end
