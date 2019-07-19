43c62cdb92a5e9b2d2f371e7531c6f1a60e8d764
Andrei Trifonov <andrei_trifonov@epam.com>
2019-07-19 15:44:58 +0300

    Fix archive name

--- src/components/sitecore/chef/cookbooks/scp_sitecore_modules/attributes/jss_1110_sc91.rb
+++ src/components/sitecore/chef/cookbooks/scp_sitecore_modules/attributes/jss_1110_sc91.rb
@@ -2,7 +2,7 @@ default['scp_sitecore_modules']['jss_1110_sc91'] = {
   'config' => {
     'module' => 'jss',
     'package_full_path' => 'C:/tmp/sitecore_modules/Sitecore_JSS.zip',
-    'package_original_name' => 'Sitecore JavaScript Services Server for Sitecore 9.1.1 XP 11.1.0 rev. 090719.scwdp.zip',
+    'package_original_name' => 'Sitecore JavaScript Services Server for Sitecore 9.1.1 XP 11.1.0 rev. 190508.scwdp.zip',
     'package_url' => 'https://dev.sitecore.net/~/media/17A6875A25014961940D84FA9884EFF0.ashx',
   },
 }
