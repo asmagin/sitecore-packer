edd12e773cc0bd8af5fd66ca2d28e6b7bf6bfd32
Igor Zharikov <zharikovigor97@gmail.com>
2019-08-01 13:34:18 +0300

    Fix SPE version for Sitecore XP v 9.2.0

--- src/containers/sc920/chef/cookbooks/scp_packer_sc920/recipes/install.rb
+++ src/containers/sc920/chef/cookbooks/scp_packer_sc920/recipes/install.rb
@@ -1,4 +1,4 @@
 include_recipe 'scp_sitecore_92x::920xp'
-include_recipe 'scp_sitecore_modules::spe'
+include_recipe 'scp_sitecore_modules::spe_sc92'
 include_recipe 'scp_sitecore_modules::spe_remoting'
 include_recipe 'scp_develop::debugger'
