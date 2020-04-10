65762a795941c6fed73507ff29612c5b28771aee
Anton Kuryan <Anton_Kuryan@epam.com>
2020-04-10 13:55:32 +0300

    Now TLS 1.2 is default

--- src/containers/w/packer/provisioners/chef/upload/prepare.ps1
+++ src/containers/w/packer/provisioners/chef/upload/prepare.ps1
@@ -1,3 +1,8 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+Write-Host "Allowing TLS 1.2 to be used"
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord
+
 Write-Host "Installing Chef Client"
 # We might want to bump it later. Testing required
 choco install chef-client -y --version 13.4.24 #--version 15.0.300 --force
